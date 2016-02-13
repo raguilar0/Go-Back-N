468f328637cc22ce801f82e84060628eb6288ec9
raguilar0 <ricardoaguilar0@gmail.com>
2016-02-12 20:24:29 -0600

    Envio de mensajes

--- Cliente.py
+++ Cliente.py
@@ -22,6 +22,10 @@ nombreArchivo = sys.argv[2]
 puertoCliente = int(sys.argv[3])
 modo = int(sys.argv[4])
 timeout = int(5)
+ventanaInicio = 0
+ventanaFinal = 0
+archivo = open(nombreArchivo)
+contador = 0
 server_address = ('localhost', puertoCliente)
 print >>sys.stderr, 'conectando a %s puerto %s' % server_address
 sock.connect(server_address)
@@ -29,38 +33,72 @@ sock.connect(server_address)
 def wc(filename):   
     return int(check_output(["wc", "-m", filename]).split()[0])
 
-try:
-     
-    # Enviando datos
-    archivo = open(nombreArchivo)
-    linea = archivo.read(1)
-    contador = 0
-    letras = wc(nombreArchivo)
-    print letras
-    while ((linea != '')):
-        tamLinea = len(linea)
-        for i in range (0, ventana):
-            pos = str(contador)
+
+
+
+def enviarMensajesIniciales(mContador, vf):
+    archivo.seek(mContador)
+    mLinea = archivo.read(1)
+    while ((mLinea != '') & (mContador < vf)):
+        
+        for i in range (ventanaInicio, ventanaFinal):
+            pos = str(mContador)
             lenpos = len(pos)
             while lenpos < 5:
                 pos = '0'+pos
                 lenpos = len(pos)
             pba = int(pos)
-            message = '%s:%s' % (pos, linea)
+            message = '%s:%s' % (pos, mLinea)
             print >>sys.stderr, 'enviando %s' % message
             sock.sendall(message)
-            contador = contador + 1            
-            archivo.seek(contador)
-            linea=archivo.read(1)
+            mContador = mContador + 1            
+            archivo.seek(mContador)
+            contador = mContador
+            mLinea=archivo.read(1)
+    return mContador
+
+def enviarSiguienteMensaje(mContador, vf):
+
+    archivo.seek(mContador)
+    mLinea = archivo.read(1)
+    if ((mLinea != '') & (mContador < vf)):
+        pos = str(mContador)
+        lenpos = len(pos)
+        while lenpos < 5:
+            pos = '0'+pos
+            lenpos = len(pos)
+        pba = int(pos)
+        message = '%s:%s' % (pos, mLinea)
+        print >>sys.stderr, 'enviando %s' % message
+        sock.sendall(message)
+        mContador = mContador + 1            
+        archivo.seek(mContador)
+        contador = mContador
+        mLinea=archivo.read(1)
+    return mContador
+
+
+try:
+    letras = wc(nombreArchivo)
+    # Enviando datos
+    #Inicializamos la ventana
+    ventanaFinal = ventana;
+    print 'Ventana final ', ventanaFinal
+    contador = enviarMensajesIniciales(contador,ventanaFinal)  
+    
  
     # Buscando respuesta
     amount_received = 0
-    amount_expected = letras*9
+    amount_expected = letras*7
      
     while amount_received < amount_expected:
-        data = sock.recv(9)
+        data = sock.recv(7)
         amount_received += len(data)
         print >>sys.stderr, 'recibiendo "%s"' % data
+        ventanaInicio = ventanaInicio+1
+        ventanaFinal = ventanaFinal+1
+        if(ventanaFinal < letras):
+            contador = enviarSiguienteMensaje(contador, ventanaFinal)
  
 finally:
     print >>sys.stderr, 'cerrando socket'
